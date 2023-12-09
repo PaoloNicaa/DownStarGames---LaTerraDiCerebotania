81a4d8743ef5caa67b68d6c02633f8e682741447
LaCapraFaMuu <lnodari@cerebotani.it>
2023-12-09 22:28:08 +0100

    Aggiunta di 1 possibilita su 5 che quando avvi il programma parta la pubblicita di grubhub

--- UI-Manager/SurvivalGameGUI.java
+++ UI-Manager/SurvivalGameGUI.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyListener;
 import java.util.Random;
 
 import utils.Player;
+import utils.PopUp;
 import Item.Mela;
 import Item.Stivali;
 import Item.Spada;
@@ -43,7 +44,8 @@ public class SurvivalGameGUI extends JFrame implements ActionListener, KeyListen
 
         // Caricamento immagine sfondo
         ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("background.png"));
-        backgroundLabel = new JLabel(backgroundIcon);
+        Image backgroundImage = backgroundIcon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+        backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
         backgroundLabel.setSize(getSize());
 
         // Creazione pannello layered
@@ -204,7 +206,12 @@ public class SurvivalGameGUI extends JFrame implements ActionListener, KeyListen
 
             game.setIconImage(image);
             game.setVisible(true);
-            
+
+            Random random = new Random();
+            int popup = random.nextInt(0,5); // 1 possibilita su 5 per far partire la pubblicita di grubhub
+            if (popup == 1) {
+                new PopUp();
+            }
         });
     }
 }
---
+++ UI-Manager/utils/PopUp.java
@@ -0,0 +1,27 @@
+package utils;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class PopUp {
+
+    private final String videoPath;
+
+    public PopUp() {
+        String nomeFile = "ad.mp4";
+        String directoryCorrente = System.getProperty("user.dir"); // Path della directory di lavoro corrente
+        directoryCorrente += "\\utils\\"; // Aggiunta sottocartella utils
+        File file = new File(directoryCorrente, nomeFile);
+        String percorsoAssoluto = file.getAbsolutePath(); // Trasformo in stringa il totade del path
+        this.videoPath = percorsoAssoluto;
+        playVideo();
+    }
+
+    private void playVideo() {
+        try {
+            Desktop.getDesktop().open(new File(videoPath)); // Apertura del file da video player di windows
+        } catch (IOException e) {
+            e.printStackTrace(); // Print dell'errore in caso il file non venga trovato
+        }
+    }
+}
